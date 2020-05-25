d1f60fa731380dcc0edf021ce9890a30a736938e
alles-gut <jhu4142@gmail.com>
2020-05-25 14:59:30 +0900

    Task 2

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import random
 from dataio import *
 from conv import *
 from ft import *
+from psnr import *
 
 def generateNoise(size):
     print("Generating noise")
     noise = np.zeros(size)
     for i in range(size[0]):
-        for i in range(size[1]):
+        for j in range(size[1]):
             noise[i,j] = random.randint(0,255)
     return noise/255
 
@@ -32,21 +33,30 @@ if __name__ == '__main__':
     print("DFT blur kernel")
     ft_blur_kernel = ft(blur_kernel, blur_img.shape)
     print("DFT blurred image")
-    ft_blur_img    = ft(blur_img, blur_img.shape)
-    print("IDFT performing")
-    guess_original = invFt(ft_blur_img / ft_blur_kernel)[0:orig_img.shape[0], 0:orig_img.shape[1]]
-    guess_original = abs(guess_original) / (np.sum(blur_img)/blur_img.size) / 255
+    ft_blur_img = ft(blur_img, blur_img.shape)
+    print("IDFT performing - ft_blur_img / ft_blur_kernel")
+    deblur = invFt(ft_blur_img / ft_blur_kernel)[0:orig_img.shape[0], 0:orig_img.shape[1]]
+    deblur = abs(deblur) / blur_img.size / 255
     print("Task 1 done")
 
     #Task 2
     print("Task 2")
-    noise = generateNoise([blur_img.shape])
-    ft_noise = ft(noise, blur_img.shape)
-    ft_orig_img = ft(orig_img, blur_img.shape)
-    blur_img_noised = invFt(ft_orig_img * ft_blur_kernel + ft_noise)
+    blur_img_noised = blur_img + noise
+    print("DFT blur_img_noised")
+    ft_blur_img_noised = ft(blur_img_noised, blur_img.shape)
+    print("IDFT performing - deblur noise/blurred image")
+    noise_deblur = invFt((abs(ft_blur_kernel) > 6*1e-8)*ft_blur_img_noised/ft_blur_kernel)
+    noise_deblur = abs(inv)[0:img.shape[0], 0:img.shape[1]]/blur_img.size
  
+
+    #Task 3
+    print("Task 3")
+    psnr_task1 = 
+    psnr_task2 = 
+
+
     #show results
-    image_print_name_list  = ['Original Image', 'Task1 - Blurred Image', 'Task1 - Guess Original']
-    image_print_image_list = [ orig_img, blur_img, guess_original]
+    image_print_name_list  = ['Original Image', 'Task1 - Blurred Image', 'Task1 - Guess Original', ]
+    image_print_image_list = [ orig_img, blur_img, deblur, blur_img_noised]
     showImage(image_print_name_list, \
             image_print_image_list)
---
+++ psnr.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#calculate PSNR of img image for base image
+def psnr(base, obj):
+    if base.shape != obj.shape :
+        print("ERROR! shape of input images are not match")
+        return -1
+
+    mse = np.sum(base ** 2 - obj ** 2) / base.size
+    psnr = 10 * np.log10(1/mse)
+
+    return psnr
