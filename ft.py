bbe7a1deb220703219bcdb88e8acd0cc0aa6f2ba
alles-gut <jhu4142@gmail.com>
2020-05-27 15:48:36 +0900

    FT function efficiency improved

--- ft.py
+++ ft.py
@@ -14,19 +14,21 @@ def ft (arr, size):
         row_arr = size[1]
         size_arr = col_arr * row_arr
 
-    m = np.ones([col_arr, row_arr])
-    n = np.ones([col_arr, row_arr])
+    m = np.ones([col_arr, col_arr])
+    wm = np.ones([col_arr, col_arr])
+    n = np.ones([row_arr, row_arr])
+    wn = np.ones([row_arr, row_arr])
     for i in range(col_arr):
         m[i] = -2 * np.pi * m[i] * i / col_arr
+        wm[:,i] = i
     for i in range(row_arr):
         n[:,i] = -2 * np.pi * n[:,i] * i / row_arr
+        wn[i] = i
+    wm = np.exp(wm * m * 1j)
+    wn = np.exp(wn * n * 1j)
 
     ft_arr = np.zeros([col_arr, row_arr],dtype=complex)
-    for u in range(int(col_arr)):
-        for v in range(int(row_arr)):
-            if u % 50 == 0 and v == 0:
-                print("iteration :",u, v)
-            ft_arr[u,v] = np.sum( arr * np.exp( ( m * u + n * v ) * 1j )) / size_arr
+    ft_arr = np.matmul(np.matmul(wm, arr), wn) / size_arr
 
     return ft_arr
 
@@ -35,20 +37,21 @@ def ft (arr, size):
 def invFt (arr):
     col_arr  = arr.shape[0]
     row_arr  = arr.shape[1]
-    size_arr = arr.size
 
-    m = np.ones([col_arr, row_arr])
-    n = np.ones([col_arr, row_arr])
+    m = np.ones([col_arr, col_arr])
+    wm = np.ones([col_arr, col_arr])
+    n = np.ones([row_arr, row_arr])
+    wn = np.ones([row_arr, row_arr])
     for i in range(col_arr):
         m[i] = 2 * np.pi * m[i] * i / col_arr
+        wm[:,i] = i
     for i in range(row_arr):
         n[:,i] = 2 * np.pi * n[:,i] * i / row_arr
+        wn[i] = i
+    wm = np.exp(wm * m * 1j)
+    wn = np.exp(wn * n * 1j)
 
     invft_arr = np.zeros([col_arr, row_arr],dtype=complex)
-    for u in range(int(col_arr)):
-        for v in range(int(row_arr)):
-            if u % 50 == 0 and v == 0:
-                print("Iteration :", u, v)
-            invft_arr[u,v] = np.sum( arr * np.exp( ( m * u + n * v ) * 1j ))
+    invft_arr = np.matmul(np.matmul(wm, arr), wn)
 
     return invft_arr
